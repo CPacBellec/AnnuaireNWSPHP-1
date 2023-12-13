1c258be46f714fd1a6ec316d05d22b8ff231d18f
Cyril Bellec <cbellec@normandiewebschool.fr>
2023-12-13 16:47:34 +0100

    update form

--- templates/includes/form_add.inc.php
+++ templates/includes/form_add.inc.php
@@ -4,7 +4,7 @@ $database = new Database($connection);
 
 if(isset($_POST['submit'])){
   if(!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['birthday']) && !empty($_POST['email']) && !empty($_POST['phone'])){
-    $payload = [$_POST['name'],$_POST['surname'],$_POST['birthday'],$_POST["email"],$_POST['phone'],$_POST['address'],$_POST['postalcode'],$_POST['city'],$_POST['formation']];
+    $payload = [$_POST['name'],$_POST['surname'],$_POST['birthday'],$_POST["email"],$_POST['phone'],$_POST['address'],$_POST['postalcode'],$_POST['city'],$_POST['class']];
     $database->create($payload);
     header('Location: ./?page=home&layout=html');
     exit;
